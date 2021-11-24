70645ae3f277ee20686e6118ddc8c6c814d769fd
andyruan <andyruan@outlook.com>
2021-11-24 01:50:13 -0500

    Added Table

--- gui/gui.py
+++ gui/gui.py
@@ -106,6 +106,39 @@ class MyTableWidget(QWidget):
         self.tab3 = QWidget()
         self.tabs.resize(300,200)
         
+
+        # Add tabs to widget
+        self.layout.addWidget(self.tabs)
+        self.setLayout(self.layout)
+        header = ['开关', '只平', '期货账号', '策略编号', '交易合约', '总持仓', '买持仓', '卖持仓', '持仓盈亏', '平仓盈亏', '手续费', '净盈亏', '成交量', '成交金额', 'A成交率', 'B成交率', '交易模型', '下单算法']
+        dataList = [
+                [QCheckBox("关"), 0, '063802', '01', 'rb1705,rb1710', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'MA', '01'],
+                [QCheckBox("关"), 0, '063802', '02', 'cu1705,cu1710', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'MA', '01'],
+                [QCheckBox("关"), 0, '063802', '03', 'zn1705,zn1710', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'MA', '01'],
+                [QCheckBox("关"), 0, '063802', '04', 'rb1705,rb1710', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'MA', '01'],
+                [QCheckBox("关"), 0, '063802', '01', 'zn1705,zn1710', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'MA', '01'],
+                [QCheckBox("关"), 0, '063802', '02', 'ru1705,ru1710', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'MA', '01'],
+                [QCheckBox("关"), 0, '063802', '02', 'ni1705,ni1710', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'MA', '01'],
+                [QCheckBox("关"), 0, '063802', '01', 'rb1705,rb1710', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'MA', '01'],
+            ]
+
+        self.table_model = MyTableModel(self, dataList, header)
+        self.table_view = QTableView()
+        #self.table_view.setSelectionMode(QAbstractItemView.SingleSelection)
+        self.table_view.setSelectionMode(QAbstractItemView.SingleSelection)
+        self.table_view.setSelectionBehavior(QAbstractItemView.SelectRows)
+        # bind cell click to a method reference
+        self.table_view.clicked.connect(self.showSelection)
+        self.table_view.clicked.connect(self.selectRow)
+
+        self.table_view.setModel(self.table_model)
+        # enable sorting
+        self.table_view.setSortingEnabled(True)
+
+        layout = QVBoxLayout(self)
+        layout.addWidget(self.table_view)
+        self.setLayout(layout)
+
         # Add tabs
         self.tabs.addTab(self.tab1,"Devices List")
         self.tabs.addTab(self.tab3,"Tutorial")
@@ -114,12 +147,29 @@ class MyTableWidget(QWidget):
         self.tab1.layout = QVBoxLayout(self)
         self.pushButton1 = QPushButton("PyQt5 button")
         self.tab1.layout.addWidget(self.pushButton1)
+        self.tab1.layout.addWidget(self.table_view)
         self.tab1.setLayout(self.tab1.layout)
-        
-        # Add tabs to widget
-        self.layout.addWidget(self.tabs)
-        self.setLayout(self.layout)
-        
+
+
+
+    def update_model(self, datalist, header):
+        self.table_model2 = MyTableModel(self, dataList, header)
+        self.table_view.setModel(self.table_model2)
+        self.table_view.update()
+
+    def showSelection(self, item):
+        cellContent = item.data()
+        # print(cellContent)  # test
+        sf = "You clicked on {}".format(cellContent)
+        # display in title bar for convenience
+        self.setWindowTitle(sf)
+
+    def selectRow(self, index):
+        # print("current row is %d", index.row())
+        pass
+
+
+
     @pyqtSlot()
     def on_click(self):
         print("\n")
@@ -127,18 +177,149 @@ class MyTableWidget(QWidget):
             print(currentQTableWidgetItem.row(), currentQTableWidgetItem.column(), currentQTableWidgetItem.text())
 
 
+class MyTableModel(QAbstractTableModel):
+    """
+    keep the method names
+    they are an integral part of the model
+    """
+    def __init__(self, parent, mylist, header, *args):
+        QAbstractTableModel.__init__(self, parent, *args)
+        self.mylist = mylist
+        self.header = header
+        self.timer = QtCore.QTimer()
+        self.change_flag = True
+        self.timer.timeout.connect(self.updateModel)
+        self.timer.start(1000)
+        
+        # self.rowCheckStateMap = {}
+
+    def setDataList(self, mylist):
+        self.mylist = mylist
+        self.layoutAboutToBeChanged.emit()
+        self.dataChanged.emit(self.createIndex(0, 0), self.createIndex(self.rowCount(0), self.columnCount(0)))
+        self.layoutChanged.emit()
+
+    def updateModel(self):
+        dataList2 = []
+        if self.change_flag is True:
+            dataList2 = [
+                [QCheckBox("关"), 0, '063802', '01', 'rb1705,rb1710', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'MA', '01'],
+                [QCheckBox("关"), 0, '063802', '02', 'cu1705,cu1710', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'MA', '01'],
+                [QCheckBox("关"), 0, '063802', '03', 'zn1705,zn1710', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'MA', '01'],
+                [QCheckBox("关"), 0, '063802', '04', 'rb1705,rb1710', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'MA', '01'],
+                [QCheckBox("关"), 0, '063802', '01', 'zn1705,zn1710', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'MA', '01'],
+                [QCheckBox("关"), 0, '063802', '02', 'ru1705,ru1710', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'MA', '01'],
+                [QCheckBox("关"), 0, '063802', '02', 'ni1705,ni1710', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'MA', '01'],
+                [QCheckBox("关"), 0, '063802', '01', 'rb1705,rb1710', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'MA', '01'],
+            ]
+            self.change_flag = False
+        elif self.change_flag is False:
+            dataList2 = [
+                [QCheckBox("关"), 0, '058176', '01', 'rb1705,rb1710', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'MA', '01'],
+                [QCheckBox("关"), 0, '058176', '02', 'cu1705,cu1710', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'MA', '01'],
+                [QCheckBox("关"), 0, '058176', '03', 'zn1705,zn1710', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'MA', '01'],
+                [QCheckBox("关"), 0, '058176', '04', 'rb1705,rb1710', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'MA', '01'],
+                [QCheckBox("关"), 0, '058176', '01', 'zn1705,zn1710', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'MA', '01'],
+                [QCheckBox("关"), 0, '058176', '02', 'ru1705,ru1710', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'MA', '01'],
+                [QCheckBox("关"), 0, '058176', '02', 'ni1705,ni1710', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'MA', '01'],
+                [QCheckBox("关"), 0, '058176', '01', 'rb1705,rb1710', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'MA', '01'],
+            ]
+            self.change_flag = True
+
+        self.mylist = dataList2
+        self.layoutAboutToBeChanged.emit()
+        self.dataChanged.emit(self.createIndex(0, 0), self.createIndex(self.rowCount(0), self.columnCount(0)))
+        self.layoutChanged.emit()
+
+    def rowCount(self, parent):
+        return len(self.mylist)
+
+    def columnCount(self, parent):
+        return len(self.mylist[0])
+
+    def data(self, index, role):
+        if not index.isValid():
+            return None
+        if (index.column() == 0):
+            value = self.mylist[index.row()][index.column()].text()
+        else:
+            value = self.mylist[index.row()][index.column()]
+        if role == QtCore.Qt.EditRole:
+            return value
+        elif role == QtCore.Qt.DisplayRole:
+            return value
+        elif role == QtCore.Qt.CheckStateRole:
+            if index.column() == 0:
+                # print(">>> data() row,col = %d, %d" % (index.row(), index.column()))
+                if self.mylist[index.row()][index.column()].isChecked():
+                    return QtCore.Qt.Checked
+                else:
+                    return QtCore.Qt.Unchecked
+
+    def headerData(self, col, orientation, role):
+        if orientation == Qt.Horizontal and role == Qt.DisplayRole:
+            return self.header[col]
+        return None
+
+    def sort(self, col, order):
+        """sort table by given column number col"""
+        # print(">>> sort() col = ", col)
+        if col != 0:
+            self.emit(SIGNAL("layoutAboutToBeChanged()"))
+            self.mylist = sorted(self.mylist, key=operator.itemgetter(col))
+            if order == Qt.DescendingOrder:
+                self.mylist.reverse()
+            self.emit(SIGNAL("layoutChanged()"))
+
+    def flags(self, index):
+        if not index.isValid():
+            return None
+        # print(">>> flags() index.column() = ", index.column())
+        if index.column() == 0:
+            # return Qt::ItemIsEnabled | Qt::ItemIsSelectable | Qt::ItemIsUserCheckable
+            return QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsUserCheckable
+        else:
+            return QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsSelectable
+
+    def setData(self, index, value, role):
+        if not index.isValid():
+            return False
+        # print(">>> setData() role = ", role)
+        # print(">>> setData() index.column() = ", index.column())
+        # print(">>> setData() value = ", value)
+        if role == QtCore.Qt.CheckStateRole and index.column() == 0:
+            print(">>> setData() role = ", role)
+            print(">>> setData() index.column() = ", index.column())
+            if value == QtCore.Qt.Checked:
+                self.mylist[index.row()][index.column()].setChecked(True)
+                self.mylist[index.row()][index.column()].setText("开")
+                # if studentInfos.size() > index.row():
+                #     emit StudentInfoIsChecked(studentInfos[index.row()])     
+            else:
+                self.mylist[index.row()][index.column()].setChecked(False)
+                self.mylist[index.row()][index.column()].setText("关")
+        else:
+            print(">>> setData() role = ", role)
+            print(">>> setData() index.column() = ", index.column())
+        # self.emit(SIGNAL("dataChanged(QModelIndex,QModelIndex)"), index, index)
+        print(">>> setData() index.row = ", index.row())
+        print(">>> setData() index.column = ", index.column())
+        self.dataChanged.emit(index, index)
+        return True
+
+
+
 
 def rest_server(widget):
-    CLOCK_RATE_SECONDS = 1
+    CLOCK_RATE_SECONDS = 5
     BASE_URL = "http://10.6.131.127:5000/"
     while True:
         print(time.time())
-        time.sleep(CLOCK_RATE_SECONDS) # polling frequency is 10ms
         # This will return a dictionary containing the state of all the relay groups.
         response = requests.get(BASE_URL + "status-dict")
         relay_status = response.json()
         widget.relay_status = relay_status
-
+        time.sleep(CLOCK_RATE_SECONDS) # polling frequency is 10ms
 
 if __name__ == '__main__':
 
